cab505717092499f905df244c3c525dfcdad5ae5
Admin-Yj <1662324535@qq.com>
2022-02-24 16:08:19 +0800

    1.0.0.0

---
+++ entity/src/main/java/com/gyhb/entity/bo/UserVo.java
@@ -0,0 +1,41 @@
+package com.gyhb.entity.bo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "用户对象BO", description = "从客户端，由用户传入的数据封装在此entity中")
+public class UserVo {
+
+    @ApiModelProperty(value = "微信号", name = "wechatNumber", example = "wx00001", required = true)
+    private String wechatNumber;
+    @ApiModelProperty(value = "密码", name = "password", example = "123456", required = true)
+    private String password;
+    @ApiModelProperty(value = "确认密码", name = "confirmPassword", example = "123456", required = false)
+    private String confirmPassword;
+
+    public String getWechatNumber() {
+        return wechatNumber;
+    }
+
+    public void setWechatNumber(String wechatNumber) {
+        this.wechatNumber = wechatNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+
+}
